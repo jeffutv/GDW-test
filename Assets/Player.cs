901b3a4a0934921e960d3c1c2a31b306b68a2aeb
Jeff Warmouth <jeffutv>
2017-10-31 12:10:05 -0400

    Add Unity Assets & ProjectSettings
    Make this into a Unity Project Folder

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+		Vector3 move = new Vector3 (moveH, 0.0f, moveV);
+		rb.AddForce (move * speed);
+	}
+}
